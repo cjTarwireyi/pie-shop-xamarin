a6911f692cf0e27d30dcab95d3468d7cf858fc19
cornelious Tarwireyi <cornelioustarwireyi@gmail.com>
2022-11-30 23:37:12 +0200

    first view creation

--- PieShopMobile/PieShopMobile/MainActivity.cs
+++ PieShopMobile/PieShopMobile/MainActivity.cs
@@ -1,7 +1,9 @@
 ﻿using Android.App;
 using Android.OS;
 using Android.Runtime;
+using Android.Widget;
 using AndroidX.AppCompat.App;
+using System;
 
 namespace PieShopMobile
 {
@@ -14,7 +16,16 @@ namespace PieShopMobile
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+            Button myButton = FindViewById<Button>(Resource.Id.MyButton);
+            myButton.Click += MyButton_Click;
         }
+
+        private void MyButton_Click(object sender, EventArgs e)
+        {
+            var toast = Toast.MakeText(this, "A button was clicked", ToastLength.Short);
+            toast.Show();
+        }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
