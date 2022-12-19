59ad4a9aa1f3cc92e9cf227840a3158bcaf3b28e
cornelious Tarwireyi <cornelioustarwireyi@gmail.com>
2022-12-19 13:37:24 +0200

    API project initial set up

---
+++ PieShopMobile/PieShop.API/Model/ShoppingCartItem.cs
@@ -0,0 +1,17 @@
+﻿namespace PieShopMobile.API.Model
+{
+    public class ShoppingCartItem
+    {
+        public Pie Pie
+        {
+            get;
+            set;
+        }
+
+        public int Amount
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ PieShopMobile/PieShop.API/Repository/ShoppingCartRepository.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using   PieShopMobile.API.Model;
+
+namespace   PieShopMobile.API.Repository
+{
+    public class ShoppingCartRepository
+    {
+        private static ShoppingCart _shoppingCart = new ShoppingCart();
+
+        public void AddToShoppingCart(Pie pie, int amount)
+        {
+            var shoppingCartItem = new ShoppingCartItem()
+            {
+                Pie =  pie,
+                Amount =  amount
+            };
+
+            _shoppingCart.CartItems.Add(shoppingCartItem);
+        }
+
+        public List<ShoppingCartItem> GetAllShoppingCartItems()
+        {
+
+            return _shoppingCart.CartItems;
+        }
+
+    }
+}
